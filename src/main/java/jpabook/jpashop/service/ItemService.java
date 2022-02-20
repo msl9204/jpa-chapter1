3e853cf0cc6cd10da732559d105fd583a7ff0d80
minsoo.lee <letomok@gmail.com>
2022-02-21 00:52:20 +0900

    웹계층 개발

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,18 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) {
+
+        // 영속상태의 아이템을 찾아와서
+        Item findItem = itemRepository.findOne(itemId);
+
+        // 값을 넣으면 영속상태의 엔티티이기 때문에 dirty checking이 이루어지고 변경된 값이 들어간다.
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
     }
 
     public List<Item> findItems() {
